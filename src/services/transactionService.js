45e1b75359bfca11dbb4027d0c53550c27357e9f
Your Name <you@example.com>
2023-06-23 23:19:52 -0500

    Change: saldo de usuario

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -13,6 +13,7 @@ class transactionService extends BaseService {
     constructor() {
         super(Transaction);
         this.userServ = new UserService();
+        this.baseServ = new BaseService(User);
     }
 
     async deposit(userId, id, amount) {
@@ -65,19 +66,14 @@ class transactionService extends BaseService {
     }
 
 
-    async getBalance(userId, id) {
+
+    async getBalanceUser(userId, id) {
         const { results: user } = await this.userServ.userOne(userId, id);
-
-        const deposit = await Transaction.sum('amount', { where: { userId: id, category: 'Deposit' } });   // suma de los depositos 
-        const withdraw = await Transaction.sum('amount', { where: { userId: id, category: 'Withdraw' } }); // suma de los retiros
-        const bet = await Transaction.sum('amount', { where: { userId: id, category: 'Bet' } });            // suma de las apuestas
-        const winning = await Transaction.sum('amount', { where: { userId: id, category: 'Winning' } });    // suma de las ganancias
-        const balance = (deposit + winning) - (withdraw + bet); // balance actual del usuario 
-
+        const balance = await this.#balance(id);
         return {
             userId: id,
-            balance: balance,
-            message: `The current balance of user ${user.firstName} ${user.lastName} is $ ${balance}`,
+            balance: balance.balance,
+            message: `The current balance of user ${user.firstName} ${user.lastName} is $ ${balance.balance}`,
         }
     }
 
@@ -166,14 +162,41 @@ class transactionService extends BaseService {
         }
     }
 
-    //solicitar saoldo de un usuario
-    async getBalanceUsserAdmin(userId, idUser) {
-        console.log(userId);
-        console.log(idUser);
+
+    //solicitar saldo de un usuario poe username
+    async getBalanceUserAdmin(userId, idUser, username) {
+        const { results: user } = await this.userServ.userOne(userId, idUser);
+
+        const { entities: userBalance } = await this.baseServ.findAllByField('username', username)
+        if (!userBalance) throw new Error(`The user ${username} does not exist`);
+
+        const balance = await this.#balance(userBalance[0].id);
+
 
         return {
-            userId,
-            idUser,
+            username: userBalance[0].username,
+            firstName: userBalance[0].firstName,
+            lastName: userBalance[0].lastName,
+            balance: balance.balance,
+            message: `The current balance of user ${userBalance[0].firstName} ${userBalance[0].lastName} is $ ${balance.balance}`,
+        }
+
+
+    }
+
+
+    //consultar saldo de un usuario por id
+    async #balance(id) {
+        const deposit = await Transaction.sum('amount', { where: { userId: id, category: 'Deposit' } });   // suma de los depositos 
+        const withdraw = await Transaction.sum('amount', { where: { userId: id, category: 'Withdraw' } }); // suma de los retiros
+        const bet = await Transaction.sum('amount', { where: { userId: id, category: 'Bet' } });            // suma de las apuestas
+        const winning = await Transaction.sum('amount', { where: { userId: id, category: 'Winning' } });    // suma de las ganancias
+        const balance = (deposit + winning) - (withdraw + bet); // balance actual del usuario 
+
+        return {
+            userId: id,
+            balance: balance,
+            // message: `The current balance of user ${user.firstName} ${user.lastName} is $ ${balance}`,
         }
     }
 }
